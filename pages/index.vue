e5c282d4a50f4fc30fe9c0932f3814787ae8566b
Leo <leonardomartins578@gmail.com>
2022-08-17 21:24:55 -0300

    primeiro commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Nav />
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  async created() {
+    this,
+      (this.sale_items = await this.$content("products")
+        .where({ onSale: true })
+        .fetch());
+    this.products = await this.$content("products").fetch();
+  },
+  data() {
+    return {
+      products: null,
+      sale_items: null,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
